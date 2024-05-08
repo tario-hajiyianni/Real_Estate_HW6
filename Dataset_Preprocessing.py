c3ec6f411f4ff842455312172b3c8be02f1ea0d9
Tario <tario2020@gmail.com>
2024-05-08 12:58:52 +0300

    dropping columns , creating two dataframes with one with the location (x,y co-ords) and one without

--- Dataset_Preprocessing.py
+++ Dataset_Preprocessing.py
@@ -1,5 +1,24 @@
 import pandas as pd
 
 # Load the dataset
-data = pd.read_excel("Real_Estate_Sales_2001-2020_GL.xlsx")
+data = pd.read_excel("C:/Users/Tario/Desktop/UNIC/SM5/COMP-340-1 Big Data/HW6/Real_Estate_HW6/Real_Estate_Sales_2001-2020_GL.xlsx")
 
+columns_to_drop = ["Non Use Code", "Assessor Remarks", "OPM remarks", "Date Recorded"]
+data.drop(columns=columns_to_drop, inplace=True)
+print("Count of rows after dropping columns:", len(data))
+
+# Splitting the dataframe based on the 'Location' column
+data_with_location = data[data['Location'].notna()]
+data_without_location = data[data['Location'].isna()]
+
+print("Count of rows in dataframe with Location:", len(data_with_location))
+print("Count of rows in dataframe without Location:", len(data_without_location))
+
+
+# Extract X and Y coordinates from the 'Location' column
+#data['X'] = data['Location'].apply(lambda x: float(x.split(' ')[1][1:]))
+#data['Y'] = data['Location'].apply(lambda x: float(x.split(' ')[2][:-1]))
+
+#print(data.head())
+
+#data.to_csv("", index=False)
