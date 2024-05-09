43c0ac76d472262c3043f5a5048da7f1581baeef
Tario <tario2020@gmail.com>
2024-05-09 13:09:56 +0300

    Spliting Location into parts

--- Dataset_Preprocessing.py
+++ Dataset_Preprocessing.py
@@ -21,10 +21,19 @@ data_with_location = data_with_location.dropna()
 print("Count of rows in dataframe with Location after dropping N/A values:", len(data_with_location))
 print("Count of rows in dataframe without Location after dropping N/A values:", len(data_without_location))
 
-# Extract X and Y coordinates from the 'Location' column
-#data['X'] = data['Location'].apply(lambda x: float(x.split(' ')[1][1:]))
-#data['Y'] = data['Location'].apply(lambda x: float(x.split(' ')[2][:-1]))
+#Count of rows after dropping columns: 997213
+#Count of rows in dataframe with Location: 197697
+#Count of rows in dataframe without Location: 799516
+#Count of rows in dataframe with Location after dropping N/A values: 123755
+#Count of rows in dataframe without Location after dropping N/A values: 799516
 
-#print(data.head())
+#Extract X and Y coordinates from the 'Location' column eg POINT (-72.8449 41.32326)
+data_with_location['X'] = data_with_location['Location'].apply(lambda x: float(x.split(' ')[1][1:]))
+data_with_location['Y'] = data_with_location['Location'].apply(lambda x: float(x.split(' ')[2][:-1]))
+
+print(data_with_location.head())
+# Town            Address   Location         X         Y
+# Vernon     27 TRACY DRIVE POINT (-72.47857 41.84938) -72.47857  41.84938
+#Actual 41°50'58"N 72°28'42"W
 
 #data.to_csv("", index=False)
